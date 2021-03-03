e67262bdd56f2a448171164e056c075ed9466853
junfeng <junfeng.chen@mines-paristech.fr>
2021-03-03 18:40:02 +0100

    qualitative method

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # twin_decoder
+This is the repository for the article "A twin-decoder structure for incompressible laminar flow reconstruction with uncertainty estimation around 2D obstacles".![architecture](./images/architecture.png)
+
+The entire project are validated in **Ubuntu 20.04**.
+## Package requisitions
+- **Python** = 3.6.9
+- **Tensorflow** = 2.0
+
+## Structure of the repository
+- **dataset_utils** : general functions, including data pre-processing, results plotting etc..
+- **network_utils** : layers and models used in the article;
+- **params** : directions, network hyper-parameters etc..
+- **train_predict** : load data set and train a model, save the learning history and the mean squared error over the whole data set;
+- **mistake_minimization** : an independent script find the optimal threshold of the reconstruction error for the qualitative method;
+- **nll_minimization** : an independent script solving problem (10) for the quantitative trust level method;
+- **Depth5** : results direction saving the model, and the mean squared errors in csv format.
+
+## Model training
+Specify model's name in ```train_predict.py``` by choosing one of the four models:
+```
+Twin_AE5, Twin_AE6, Dual_AE, U_Dual_AE
+```
+To train a model, run
+```
+python3 train_predict.py
+```
+To get the reconstruction threshold, run
+```
+python3 mistake_minimization.py
+```
+![qualitative](./images/qualitative.png)
+
+
+To get the regression line, run
+```
+python3 nll_minimization.py
+```
+![quantitative](./images/quantitative.png)
---
+++ mistake_minimization.py
@@ -0,0 +1,112 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+
+
+##### give a tolerance on the flow prediction MSE
+e_f_star = 5e-5
+
+mse_tr = pd.read_csv('Depth5/mse_train.csv')
+mse_tr.columns = ['index', 'shapes', 'flow']
+
+mse_va = pd.read_csv('Depth5/mse_valid.csv')
+mse_va.columns = ['index', 'shapes', 'flow']
+
+mse_te = pd.read_csv('Depth5/mse_test.csv')
+mse_te.columns = ['index', 'shapes', 'flow']
+
+###############################
+f_tr = list()
+f_va = list()
+
+interval = np.arange(1e-7, 5e-4, 1e-8)
+for e_s in interval:
+    
+    FN = (( mse_tr.shapes < e_s ) & ( mse_tr.flow > e_f_star)).sum()
+    FP = (( mse_tr.shapes > e_s ) & ( mse_tr.flow < e_f_star)).sum()
+    f_tr.append( (FN + FP) / len(mse_tr) )
+    
+    FN = (( mse_va.shapes < e_s ) & ( mse_va.flow > e_f_star)).sum()
+    FP = (( mse_va.shapes > e_s ) & ( mse_va.flow < e_f_star)).sum()
+    f_va.append( (FN + FP) / len(mse_va) )
+
+fig = plt.figure()
+ax = plt.axes([0,0,1,1])
+plt.plot(interval, f_tr, label='train')
+plt.plot(interval, f_va, label='valid')
+#plt.axis('off')
+plt.grid()
+plt.xlabel('e_s')
+plt.ticklabel_format(axis="x", style="scientific", scilimits=(0,0))
+plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+plt.title('Mistaken classification rate')
+plt.legend(loc='upper right')
+plt.savefig('images/mistake_rate.png', dpi=300, bbox_inches='tight',pad_inches = 0)
+
+
+############################
+e_s_star = interval[np.argmin(f_va)]
+print('Given the tolerance e_f* = {}, '.format(e_f_star), 'the optimal threshold is {}. '.format(e_s_star), 'Mistake rate on the validation set is {}. '.format(f_va[np.argmin(f_va)]))
+# print((( mse_va.shapes < e_s_star ) & ( mse_va.flow > e_f_star)).sum() / ( mse_va.shapes < e_s_star ).sum())
+# print((( mse_va.shapes > e_s_star ) & ( mse_va.flow < e_f_star)).sum()/ ( mse_va.shapes > e_s_star ).sum())
+###########################
+
+results = dict()
+for e_f_star in np.arange(1e-5, 1e-4, 1e-5):
+    f_tr = list()
+    f_va = list()
+    interval = np.arange(1e-6, 4e-4, 1e-7)
+    for e_s in interval:
+    
+        FN = (( mse_tr.shapes < e_s ) & ( mse_tr.flow > e_f_star)).sum()
+        FP = (( mse_tr.shapes > e_s ) & ( mse_tr.flow < e_f_star)).sum()
+        f_tr.append(( FN + FP ) / len(mse_tr))
+    
+        FN = (( mse_va.shapes < e_s ) & ( mse_va.flow > e_f_star)).sum()
+        FP = (( mse_va.shapes > e_s ) & ( mse_va.flow < e_f_star)).sum()
+        f_va.append(( FN + FP ) / len(mse_va))
+
+    #plt.plot(np.arange(1e-5, 1.5e-4, 1e-6), f_tr, label='train')
+    #plt.plot(np.arange(1e-5, 1.5e-4, 1e-6), f_va, label='valid')
+    #plt.grid()
+    #plt.xlabel('e_s_star')
+    #plt.ylabel('false classification rate')
+    #plt.legend(loc='upper right')
+    #plt.savefig('e:/Uncert_U_net/256_16_3/zoom_false_calssifation_rate.png')
+
+    index = np.argmin(f_va)
+    e_s_star = interval[index]
+
+    FN = (( mse_te.shapes < e_s_star ) & ( mse_te.flow > e_f_star)).sum()
+    FP = (( mse_te.shapes > e_s_star ) & ( mse_te.flow < e_f_star)).sum()
+
+    results[e_f_star] = np.array([e_s_star, f_tr[index], f_va[index],(FN + FP)/len(mse_te)])
+
+
+all_e_f = np.arange(1e-5, 1e-4, 1e-5)
+all_e_s = np.array(list(results.values()))[:,0]
+
+fig = plt.figure()
+ax = plt.axes([0,0,1,1])
+plt.plot(all_e_f, all_e_s)
+plt.ticklabel_format(axis="both", style="scientific", scilimits=(0,0))
+plt.grid()
+plt.xlabel('e_f_star')
+plt.title('e_s_star as a function of e_f_star')
+plt.savefig('images/threshold_versus_tolerance.png', dpi=300, bbox_inches='tight',pad_inches = 0)
+
+
+fig = plt.figure()
+ax = plt.axes([0,0,1,1])
+plt.plot(all_e_f, np.array(list(results.values()))[:,1], label='train')
+plt.plot(all_e_f, np.array(list(results.values()))[:,2], label='validation')
+plt.plot(all_e_f, np.array(list(results.values()))[:,3], label='test')
+plt.ticklabel_format(axis="x", style="scientific", scilimits=(0,0))
+plt.grid()
+plt.legend(loc='upper right')
+plt.xlabel('e_f_star')
+plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+plt.title('False classification rate as a function of e_f_star')
+plt.savefig('images/Mistake_rate_versus_tolerance.png', dpi=300, bbox_inches='tight',pad_inches = 0)
+
